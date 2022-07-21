1c018759c54835b38866d86808b9734518c271d5
Shishu Kumar Choudhary <62634899+Shishukumar74@users.noreply.github.com>
2022-07-21 23:39:06 +0530

    Add files via upload

---
+++ Find_the_Middel_Index_in_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n = nums.size();
+        int left_sum = 0;
+        int right_sum = 0;
+        for(int i=0;i<n;i++) right_sum += nums[i];
+        for(int i=0;i<n;i++){
+            right_sum -= nums[i];
+            if(right_sum==left_sum) return i;
+            left_sum += nums[i];
+        }
+        return -1;
+        
+    }
+};
